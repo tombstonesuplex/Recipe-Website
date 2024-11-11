849532028b368b20d125bf897a67e8a5c945f7ce
tombstonesuplex <wsmi448@aucklanduni.ac.nz>
2024-11-11 15:39:14 +1300

    dotnet intro

--- RecipeWebsite/Program.cs
+++ RecipeWebsite/Program.cs
@@ -1,25 +1,24 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace RecipeWebsite
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);   
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen();
+
+
+            var app = builder.Build();
+            if (app.Environment.IsDevelopment()) { app.UseSwagger(); app.UseSwaggerUI(); }
+            app.UseHttpsRedirection();
+            app.MapControllers();
+            app.Run();
+        }
+
+
+    }
+}
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
